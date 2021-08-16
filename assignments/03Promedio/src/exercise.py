df16770a6be2768f205c829a02f049f23f4b961a
ValGar20 <a01742469@tec.mx>
2021-08-16 23:21:18 +0000

    resolviendo ejercicio de promedio

--- assignments/03Promedio/src/exercise.py
+++ assignments/03Promedio/src/exercise.py
@@ -1,14 +1,14 @@
 def main():
     #escribe tu código abajo de esta línea
-    n1 = int(input('Dame la calificación: '))
-    n2 = int(input('Dame la calificación: '))
-    n3 = int(input('Dame la calificación: '))
-    n4 = int(input('Dame la calificación: '))
+    n1 = int(input('Calificación de la materia: '))
+    n2 = int(input('Calificación de la materia: '))
+    n3 = int(input('Calificación de la materia: '))
+    n4 = int(input('Calificación de la materia: '))
 
     promedio = (n1 + n2 + n3 + n4) / 4
 
 
-    print('promedio: '+str(promedio))
+    print('El promedio es: '+str(promedio))
 
 
 if __name__ == '__main__':
